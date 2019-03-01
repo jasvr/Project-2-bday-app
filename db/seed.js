624404134c8a5c9e84da7e7b5c5f98ba472a4ce2
Jasmin Vargas <jas.vrgs@gmail.com>
2019-03-01 10:50:54 -0500

    add new more guests to seed; add guest.remove at top; close mongo

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,31 @@
 const Guest = require('../models/index')
 
-const firstGuest = Guest.create({
-    firstName: "Jas",
-    lastName: "Vargas",
-    email: "abc123@gmail.com",
-    cellNumber: "703-881-1362", 
-    agree: true
+Guest.remove({}).then(function () {
+    Guest.create({
+        firstName: "Jas",
+        lastName: "Vargas",
+        email: "abc123@gmail.com",
+        cellNumber: "703-881-1354",
+        agree: true
+    });
+
+    Guest.create({
+        firstName: "Avi",
+        lastName: "Ro",
+        email: "avi123@gmail.com",
+        cellNumber: "703-991-1362",
+        agree: true
+    })
+
+    Guest.create({
+        firstName: "Jace",
+        lastName: "Bar",
+        email: "jace123@gmail.com",
+        cellNumber: "703-991-9062",
+        agree: true
+    });
 });
 
-//CB and answer: why guests in DB?
+//const firstGuest =
+
+//CB and answer: why guests vs guest in DB? db pluarlizes it 
